ee0c2212fbaab6ae051092d66035e882092b2e03
Gustavo Amaral <gugamaral@gmail.com>
2024-05-23 21:44:24 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+nomes = ['Adão', "Aline", "Erondina", "Aluízio", 'Maria', "Joabe"]
+
+# usuario informa o índice que deseja alterar
+indice = int(input('Informe o índice que deseja alterar: '))
+indice -= 1
+
+# usuário informa o novo nome
+nomes[indice] = input('Informe o novo nome: ')
+
+# exibe a lista
+for nome in nomes:
+    print(nome)
